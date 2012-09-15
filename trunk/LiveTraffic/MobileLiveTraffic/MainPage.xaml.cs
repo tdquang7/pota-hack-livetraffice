7b17d2644353d6d7c9e3dee5578a7b4da8bbcd93
hatu87@gmail.com <hatu87@gmail.com@c9f8e27a-271a-7a71-37fa-9d24dd7e51e7>
2012-09-15 04:04:18 +0000

    first commit

---
+++ trunk/LiveTraffic/MobileLiveTraffic/MainPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Controls.Maps;
+
+namespace MobileLiveTraffic
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        
+
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private double _zoom;
+
+        public double Zoom
+        {
+            get { return _zoom; }
+            set
+            {
+                //var coercedZoom = Math.Max(MinZoomLevel, Math.Min(MaxZoomLevel, value));
+                //if (_zoom != coercedZoom)
+                //{
+                //    _zoom = value;
+                //}
+            }
+        }
+
+        private void btnZoomIn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnZoomOut_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        
+    }
+}
